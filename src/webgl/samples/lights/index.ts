903e1ce7273a98ce70c9959b1e2d8500fb5d5678
Pierre Lepers <pierre.lepers@gmail.com>
2022-04-06 17:21:38 +0200

    comment lighting sample

--- src/webgl/samples/lights/index.ts
+++ src/webgl/samples/lights/index.ts
@@ -11,6 +11,7 @@ import SpotLight from "nanogl-pbr/lighting/SpotLight"
 import { vec3 } from "gl-matrix"
 import DebugDraw from "@webgl/dev/debugDraw/DebugDraw"
 import gui from "@webgl/dev/gui"
+import FloorPlane from "@webgl/engine/FloorPlane"
 
 const GltfPath = "webgl/suzanne/Suzanne.gltf"
 // const GltfPath = "webgl/fn-509_with_tactical_kit/scene.gltf"
@@ -23,6 +24,7 @@ export default class LightsScene implements IGLContextProvider, IScene {
   lighting   : Lighting
   root       : Node
   spotLight: SpotLight
+  floor: FloorPlane
   
   constructor( renderer:Renderer ){
     this.gl = renderer.gl
@@ -30,13 +32,27 @@ export default class LightsScene implements IGLContextProvider, IScene {
     this.lighting   = new Lighting( this.gl )
     this.root.add( this.lighting.root )
     this.gltfSample = new GltfScene( AssetsPath(GltfPath), this.gl, this.lighting, this.root )
-    
 
+    
+    /*
+    * add a floor plane for fun
+    */
+    this.floor = new FloorPlane( renderer.gl )
+    this.lighting.setupMaterial(this.floor.material)
+    this.floor.node.setScale(2)
+    this.root.add( this.floor.node )
+
+
+    /**
+     * remove initial ibl light added by default by Lighting
+     */
     this.lighting.lightSetup.remove( this.lighting.ibl )
-    this.lighting.lightSetup.bounds.fromMinMax( [-10, -10, -10], [10, 10, 10] )
 
+
+    /**
+     * add a Spotlight
+     */
     this.spotLight = new SpotLight()
-    
     this.spotLight.position.set([2, 2, 2])
     this.spotLight.lookAt(vec3.create())
     this.spotLight.angle = .5
@@ -46,17 +62,22 @@ export default class LightsScene implements IGLContextProvider, IScene {
     this.lighting.root.add( this.spotLight )
     this.lighting.lightSetup.add( this.spotLight )
     
-    gui.range( this.spotLight, 'z', -5, 10 )
-    gui.range( this.spotLight, 'radius', .1, 20 )
-    gui.range( this.spotLight, 'angle', .0, Math.PI/2 )
-    gui.range( this.spotLight, 'innerAngle', .0, Math.PI/2 )
-    gui.add( this.spotLight, 'castShadows')
+    const f = gui.folder('Lighting/Spot')
+    f.range( this.spotLight, 'z', -5, 10 )
+    f.range( this.spotLight, 'radius', .1, 20 )
+    f.range( this.spotLight, 'angle', .0, Math.PI/2 )
+    f.range( this.spotLight, 'innerAngle', .0, Math.PI/2 )
+    f.add( this.spotLight, 'castShadows')
     
   }
   
+
   preRender():void {
     this.spotLight.lookAt(vec3.create())
 
+    /**
+     * display spotlight guizmos in the scene
+     */
     DebugDraw.drawSpotLight( this.spotLight )
     if(  this.spotLight.castShadows ){
       DebugDraw.drawFrustum( this.spotLight.getCamera()._viewProj )
@@ -66,14 +87,27 @@ export default class LightsScene implements IGLContextProvider, IScene {
     this.root.updateWorldMatrix()
   }
 
+
   rttPass():void {
+    /**
+     * update setup material with the current lightsetup state
+     */
     this.lighting.lightSetup.prepare(this.gl);
+
+    /**
+     * render ligtmaps for lights which cast shadows
+     */
     this.lighting.renderLightmaps( ( ctx:RenderContext )=>{
+      // by default ctx.mask is OPAQUE
+      // mask can be changed here to render subset of renderabels (eg.  Mask.SHADOW_CASTER)
       this.render(ctx)
     })
+
   }
 
+
   render( context: RenderContext ):void {
+    this.floor.render( context )
     this.gltfSample.render( context )
   }
 
@@ -86,6 +120,7 @@ export default class LightsScene implements IGLContextProvider, IScene {
      * needed if shadows are enabled on lights
      */
     this.gltfSample.computeStaticBounds(this.lighting.lightSetup.bounds)
+    this.floor.node.y = this.lighting.lightSetup.bounds.min[1]
   }
 
   unload(): void {
